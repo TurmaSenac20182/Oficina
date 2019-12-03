c38f55e9cffa79ffba3259ad92571a9c45443c09
Gustavo Alves <gustavo.empresa1@gmail.com>
2019-12-02 21:08:30 -0300

    atualizando banco
    tuto cuzão

--- banco/script banco.sql
+++ banco/script banco.sql
@@ -1,4 +1,3 @@
-drop database if exists oficina;
 create database oficina;
 use oficina;
   
@@ -13,7 +12,7 @@ create table dadoCarro (
 
 create table contato(
   idContato int  not null primary key auto_increment,
-  tel1 varchar(10) not null,
+  tel1 varchar(11) not null,
   tel2 varchar(11) null,
   email varchar(100) null
 );
@@ -32,23 +31,22 @@ create table endereco (
   bairro varchar(50)  null,
   cidade varchar(50)  null,
   uf varchar(4) not null, 
-  complemnto varchar(100)
+  complemento varchar(100)
 );
 
 create table cliente (
   idCliente int  not null primary key auto_increment,
-  nome varchar(100) not null,
-  cpf varchar(11)  not null,
-  rg varchar(9)  null,
-  carro_cliente int not null, 
+  nome   varchar(100) not null,
+  cpf   varchar(11)  not null,
+  rg    varchar(9)  null,
   contato_cliente int not null,
-  endereco_cliente int not null,
-  /*ordemServico_cliente int not null,*/
+  endereco_cleinte int not null,
+  carro_cliente int not null, 
+  ordemServico_cliente int not null,
   
-  constraint FK_carro_cliente foreign key(carro_cliente) references dadoCarro(idCarro), 
-  constraint FK_contato_cliente foreign key(contato_cliente) references contato(idContato), 
-  constraint FK_endereco_cleinte foreign key(endereco_cliente) references endereco(idEndereco)
-
+  constraint FK_contato_cliente foreign key(contato_cliente) references contato(idContato),
+  constraint FK_endereco_cleinte foreign key(endereco_cleinte) references endereco(idEndereco),
+  constraint FK_carro_cliente foreign key(carro_cliente) references dadoCarro(idCarro)   
 );
 
 create table ordemServico (
@@ -57,8 +55,8 @@ create table ordemServico (
   carro_ordemServ int not null,
   servico_ordemServ int not null,
   funcionario varchar(100) not null,
-  dataEntrada date,
-  dataSaida date, 
+  dataEntrada varchar(10) NOT NULL,
+  dataSaida varchar(10) NOT NULL, 
   maoDeObra double null,
   valorTotal double not null,
   
@@ -67,5 +65,11 @@ create table ordemServico (
   constraint FK_servico_ordemServ foreign key(servico_ordemServ) references servico(idServico)  
   );
   
-  /* comando de chava estrangeira da tabela cliente vinda de ordme de serviço
-  ALTER TABLE cliente ADD CONSTRAINT FK_ordemServico_cliente FOREIGN KEY(ordemServico_cliente) REFERENCES ordemServico(idordemServico) ON DELETE CASCADE;*/
+  CREATE TABLE evento(
+  id int primary key auto_increment NOT NULL,
+  title varchar(150) NOT NULL,
+  data varchar(10) NOT NULL
+);
+  
+  /* comando de chava estrangeira da tabela cliente vinda de ordme de serviço*/
+  ALTER TABLE cliente ADD CONSTRAINT FK_ordemServico_cliente FOREIGN KEY(ordemServico_cliente) REFERENCES ordemServico(idordemServico) ON DELETE CASCADE;

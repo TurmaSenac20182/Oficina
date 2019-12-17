b8c8b95669eb22aa5ae2fd38fcb3f5cc765b7417
Gustavo Alves <gustavo.empresa1@gmail.com>
2019-12-16 22:01:56 -0300

    banco Final
    efetuado script final do banco que cluadio viu.

--- banco/script2Oretorno.sql
+++ banco/script2Oretorno.sql
@@ -1,133 +1,121 @@
 
 CREATE DATABASE oficina;
 USE `oficina` ;
-
+-- -----------------------------------------------------
+-- Table `oficina`.`funcionario`
+-- -----------------------------------------------------
+ CREATE TABLE IF NOT EXISTS `oficina`.`funcionario` (
+  `idFuncionario` INT  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+  `nomeFuncionario` VARCHAR(100) NOT NULL,
+  `usuario` VARCHAR(50) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL  
+  );
 -- -----------------------------------------------------
 -- Table `oficina`.`contato`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `db_oficina`.`contato` ;
-
-CREATE TABLE IF NOT EXISTS `db_oficina`.`contato` (
-  `idContato` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `oficina`.`contato` (
+  `idContato` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `tel` VARCHAR(12) NULL DEFAULT NULL,
   `cel` VARCHAR(13) NOT NULL,
-  `email` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`idContato`));
-
+  `email` VARCHAR(100) NULL DEFAULT NULL
+   );
 -- -----------------------------------------------------
 -- Table `Oficina2`.`endereco`
 -- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `db_oficina`.`endereco` (
-  `idendereco` INT  PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `tipoLogradouro` VARCHAR(4) NULL,
+CREATE TABLE IF NOT EXISTS `oficina`.`endereco` (
+  `idEndereco` INT  PRIMARY KEY NOT NULL AUTO_INCREMENT,  
   `logradouro` VARCHAR(100) NULL,
   `numero` INT NULL,
-  `cep` VARCHAR(9) NOT NULL,
+  `cep` VARCHAR(9)  NULL,
   `bairro` VARCHAR(100) NULL,
   `cidade` VARCHAR(100) NULL,
   `uf` VARCHAR(2) NULL,
   `complemento` VARCHAR(100) NULL
    );
-
 -- -----------------------------------------------------
 -- Table `Oficina2`.`servico`
 -- -----------------------------------------------------
-
-
-CREATE TABLE IF NOT EXISTS `db_oficina`.`servico` (
+CREATE TABLE IF NOT EXISTS `oficina`.`servico` (
   `idservico` INT  PRIMARY KEY AUTO_INCREMENT NOT NULL ,
-  `descricao` VARCHAR(45) NOT NULL,
-  `valor` VARCHAR(45) NULL
+  `nomeServico` VARCHAR(45) NOT NULL,
+  `valor` double NULL
    );
-
 -- -----------------------------------------------------
 -- Table `Oficina2`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_oficina`.`cliente` (
-  `idcliente` INT  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `oficina`.`cliente` (
+  `idCliente` INT  PRIMARY KEY  NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `cpf` VARCHAR(15) NOT NULL,
   `rg` VARCHAR(9) NOT NULL,
-  `Contato_idContato` INT NOT NULL,
+  `contato_idContato` INT NOT NULL,
   `idEndereco` INT NOT NULL,        
-  CONSTRAINT `fk_cliente_Contato` FOREIGN KEY (`Contato_idContato`)  REFERENCES `db_oficina`.`Contato` (`idContato`),   
-  CONSTRAINT `fk_cliente_endereco`FOREIGN KEY (`idEndereco`)  REFERENCES `db_oficina`.`endereco` (`idendereco`)
+  CONSTRAINT `fk_cliente_Contato` FOREIGN KEY (`contato_idContato`)  REFERENCES `oficina`.`Contato` (`idContato`),   
+  CONSTRAINT `fk_cliente_endereco`FOREIGN KEY (`idEndereco`)  REFERENCES `oficina`.`endereco` (`idEndereco`)
  
-  );
-  
+  );  
   -- -----------------------------------------------------
 -- Table `Oficina2`.`carro`
 -- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `db_oficina`.`carro` (
+CREATE TABLE IF NOT EXISTS `oficina`.`dadocarro` (
   `idcarro` INT  PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `marca` VARCHAR(30) NOT NULL,
-  `modelo` VARCHAR(30) NOT NULL,
+  `marcaVeiculo` VARCHAR(30) NOT NULL,
+  `modeloVeiculo` VARCHAR(30) NOT NULL,
   `cor` VARCHAR(45) NOT NULL,
   `placa` VARCHAR(10) NOT NULL,
   `anoCarro` VARCHAR(4) NOT NULL,
   `idCliente_carro` INT NOT NULL,
-   CONSTRAINT `fk_carro_cliente` FOREIGN KEY (`idCliente_carro`) REFERENCES `db_oficina`.`cliente` (`idcliente`)
-   );
-   
+   CONSTRAINT `fk_carro_cliente` FOREIGN KEY (`idCliente_carro`) REFERENCES `oficina`.`cliente` (`idCliente`)
+   );  
    -- -----------------------------------------------------
 -- Table `Oficina2`.`ordemServico`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_oficina`.`ordemServico` (
+CREATE TABLE IF NOT EXISTS `oficina`.`ordemServico` (
   `idordemServico` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `idcliente` INT NOT NULL,
-  `funcionario` VARCHAR(100) NOT NULL,
-  `dataEntrada` VARCHAR(15) NOT NULL,
-  `dataSaida`   VARCHAR(15) NOT NULL,
-  `valorTotal` DOUBLE NOT NULL, 
+  `idCliente` INT NOT NULL,
+  `idFuncionario` INT  NOT NULL,
+  `dataEntrada` date NOT NULL,
+  `dataSaida`   date NULL,
+  `valorTotal` DOUBLE NULL, 
   `finalizado` tinyint default false,   
-   CONSTRAINT `fk_idcliente`FOREIGN KEY (`idcliente`) REFERENCES `db_oficina`.`cliente` (`idcliente`)  
-  );
-
--- -----------------------------------------------------
--- Table `oficina`.`servico`
--- -----------------------------------------------------
-
-
-CREATE TABLE IF NOT EXISTS `db_oficina`.`servico` (
-  `idServico` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `descricao` TEXT NOT NULL,
-  `maoDeObra` VARCHAR(255) NULL DEFAULT NULL,
-  `valor` DOUBLE NULL DEFAULT NULL
-  );
-  
+   CONSTRAINT `fk_idcliente`FOREIGN KEY (`idCliente`) REFERENCES `oficina`.`cliente` (`idCliente`),
+   CONSTRAINT `fk_idfuncionario`FOREIGN KEY (`idFuncionario`) REFERENCES `oficina`.`funcionario` (`idFuncionario`)
+  );  
   -- -----------------------------------------------------
 -- Table `oficina`.`servico_has_ordemservico`
 -- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `db_oficina`.`servico_e_ordemservico` (
+CREATE TABLE IF NOT EXISTS `oficina`.`servico_e_ordemservico` (
   `servico_idServico` INT   NOT NULL,
   `ordemservico_idordemServico` INT   NOT NULL,   
-  CONSTRAINT `fk_servico` FOREIGN KEY (`servico_idServico`) REFERENCES `db_oficina`.`servico` (`idServico`),
-  CONSTRAINT `fk_ordemservico`FOREIGN KEY (`ordemservico_idordemServico`) REFERENCES `db_oficina`.`ordemservico` (`idordemServico`)
-  );  
-  
+  CONSTRAINT `fk_servico` FOREIGN KEY (`servico_idServico`) REFERENCES `oficina`.`servico` (`idServico`),
+  CONSTRAINT `fk_ordemservico`FOREIGN KEY (`ordemservico_idordemServico`) REFERENCES `oficina`.`ordemservico` (`idordemServico`)
+  );    
   -- -----------------------------------------------------
 -- Table `Oficina2`.`pre_cadastro`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_oficina`.`pre_cadastro` (
-  `id` INT PRIMARY KEY NOT NULL,
-  `nome` VARCHAR(45) NULL,
-  `marca` VARCHAR(45) NULL,
-  `modelo` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `oficina`.`pre_cadastro` (
+  `idPreCadastro` INT PRIMARY KEY NOT NULL,
+  `nomeCliente` VARCHAR(45) NULL,
+  `marcaVeicolo` VARCHAR(45) NULL,
+  `modeloVeiculo` VARCHAR(45) NULL,
   `tel` VARCHAR(45) NULL
   );
-
 -- -----------------------------------------------------
 -- Table `oficina`.`agendamento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_oficina`.`agendamento` (
-  `id` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `oficina`.`agendamento` (
+  `idAgendamento` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
   `data` VARCHAR(10) NOT NULL,
   `hora` VARCHAR(10) NULL,
   `pre_cadastro_id` INT NOT NULL,    
-  CONSTRAINT `fk_pre_cadastro1` FOREIGN KEY (`pre_cadastro_id`) REFERENCES `db_oficina`.`pre_cadastro` (`id`)
-   );
-
-
+  CONSTRAINT `fk_pre_cadastro` FOREIGN KEY (`pre_cadastro_id`) REFERENCES `oficina`.`pre_cadastro` (`idPreCadastro`)
+   );   
+   -- -----------------------------------------------------
+-- Table `oficina`.`agendamento_has_cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `oficina`.`agendamento_has_cliente` (
+  `idAgendamento` INT NOT NULL,
+  `idCliente` INT NOT NULL,  
+  CONSTRAINT `fk_id_agendamento` FOREIGN KEY (`idAgendamento`) REFERENCES `oficina`.`agendamento` (`idAgendamento`), 
+  CONSTRAINT `fk_id_cliente`   FOREIGN KEY (`idCliente`) REFERENCES `oficina`.`cliente` (`idCliente`)
+);
